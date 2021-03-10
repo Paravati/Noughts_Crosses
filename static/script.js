6d8cbd30f2b099dd111ca6ce42e4764d9eb4800f
Patrycja <pati.iwanek@gmail.com>
2021-03-10 22:30:16 +0100

    adding username in game - 1st step

--- static/script.js
+++ static/script.js
@@ -1,18 +1,65 @@
+//document.addEventListener('click', () => {
+////  let start_button = document.getElementById('clickon');
+//  let player_name = document.getElementById('fname').value;
+//  console.log(start_button);
+//  let start_button = document.getElementById('clickon');
+//  start_button.addEventListener('click', main(player_name));
+//});
+let player_name;
+let name_is_ok = false;
+let start_button = document.getElementById('clickon');
+start_button.addEventListener('click', read_name);
 
+function read_name() {
+    player_name = document.getElementById('fname').value;
+    console.log(player_name);
+    let name_is_ok = name_ok(player_name);
+    if (name_is_ok){
+        main(player_name)
+    }else
+    document.getElementById('error_message').innerText = 'No nick or nick too long';
+}
+
+document.addEventListener('click', () => {
+  let start_button = document.getElementById('clickon');
+  console.log(start_button);
+  start_button.addEventListener('click', main);
+});
+
+function main(){
+    show_the_board();
+//        //    timer_start();
+//        start_the_game();
+}
+
+function start_the_game() {
+    let cells = document.querySelectorAll('td');
+
+//    for (let card_index = 0; card_index < cards.length; card_index++)
+//    cells[card_index].addEventListener('click', () => player_puts_dot(card_index));
+}
+
+function name_ok(name){
+    if (name.length > 0 && name.length<=10){
+        return true;
+    }else{
+        return false;
+    }
+}
 //document.addEventListener('DOMContentLoaded', () => {
 //  let start_button = document.getElementById('start_button');
 //  start_button.addEventListener('click', main);
 //});
 
 
-show_the_board();
+//show_the_board();
 
-var tablica = document.getElementsByClassName("field");
-console.log(tablica);
-console.log(tablica[0]);
-console.log(tablica[0].id)
+//var tablica = document.getElementsByClassName("field");
+//console.log(tablica);
+//console.log(tablica[0]);
+//console.log(tablica[0].id)
 
-playerMove();
+//playerMove();
 
 function playerMove(){
    for(let i=0; i<tablica.length; i++){
@@ -27,34 +74,6 @@ function reply_click()
 {
     console.log("Button clicked, id "+this.id);
 }
-//document.getElementById('arr0').onclick = reply_click;
-//document.getElementById('arr1').onclick = reply_click;
-//document.getElementById('arr2').onclick = reply_click;
-
-//function player() {
-//   for (let i = 0; i < blocks.length; i++) {
-//     blocks[i].addEventListener(‘click’, rysuj {
-//         this.getElementsByTagName(‘field’)[0].classList.add(‘clicked’);
-//     })
-//   }
-//};
-
-
-
-//document.getElementById("field").addEventListener("click", function() {
-//  var tablica = document.getElementsByClassName("field");
-//    for(let i=0; i<9; i++){
-//        tablica[i].addEventListener('click', rysuj, false);
-//}
-////  myFunction(p1, p2);
-//});
-
-//for(let i=0; i<9; i++){
-//    tablica[i].addEventListener('click', rysuj, false);
-//}
-//var zmienna = document.getElementById("arr0");
-//zmienna.addEventListener('click', rysuj, false);
-
 
 function rysuj() {
     console.log("Wybrano argument arr0")
@@ -87,5 +106,7 @@ function show_the_board() {
     `;
 
     document.querySelector('.container').innerHTML = board_string;
+//    let nick_to_display = document.getElementById('nick_to_display');
+//    nick_to_display.innerHTML = `<span style="color: red;">${player_name}</span> is playing`;
 //    document.getElementById('container').innerHTML = board_string;   -> ten zapis uzywamy gdy zamiast klasy w html mamy id
 }
